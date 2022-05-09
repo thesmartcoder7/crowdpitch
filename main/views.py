fe673b4434fd6a88eca231e8182df9b2a15b1a14
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-09 23:08:00 +0300

    feature: streamline the session saving function and the logout function

--- main/views.py
+++ main/views.py
@@ -6,19 +6,9 @@ from config import Config
 app.secret_key = Config.SECRET_KEY
 
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/')
 def home():
-    if request.method == 'POST':
-        session["user"] = request.form["s-name"]
-        session["email"] = request.form["s-email"]
-        session["password"] = request.form["s-password"]
-        # if request.form["l-email"] and request.form["l-password"]:
-        #     session["email"] = request.form["l-email"]
-        #     session["password"] = request.form["l-password"]
-         
-        return redirect(url_for('user', username = session["user"]))
-    else:
-        return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/user')
@@ -27,8 +17,36 @@ def user():
         username = session["user"]
         print(username)
         return App.render(render_template('user.html', user = username))
+    elif 'email' in session:
+        username = session["email"]
+        print(username)
+        return App.render(render_template('user.html', user = username))
     else:
         return redirect(url_for('home'))
+
+
+
+@app.route('/signup', methods=["POST", "GET"])
+def signup():
+    if request.method == 'POST':
+        session["user"] = request.form["s-name"]
+        session["email"] = request.form["s-email"]
+        session["password"] = request.form["s-password"]
+        return redirect(url_for('user', username = session["user"]))
+    else:
+        return render_template('index.html')
+
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        session["email"] = request.form["l-email"]
+        session["password"] = request.form["l-password"]
+        return redirect(url_for('user', username = session["email"]))
+    else:
+        return render_template('index.html')
+
 
 
 @app.route('/logout')

a062bc1de870e8694e413fa5ce3e2bf9ff0a86ec
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-09 21:04:09 +0300

    feature: create the upvote and the downvote function

--- main/views.py
+++ main/views.py
@@ -1,5 +1,22 @@
 from flask import render_template
 from . import app
+import jyserver.Flask as jsf
+
+
+@jsf.use(app)
+class App:
+    def __init__(self):
+        self.up_count = 0
+        self.down_count = 0
+
+    def upvote(self):
+        self.up_count = self.up_count + 1
+        self.js.document.querySelector('#upvote').innerHTML = self.up_count
+
+    def downvote(self):
+        self.down_count = self.down_count + 1
+        print(self.down_count)
+        self.js.document.querySelector('#downvote').innerHTML = self.down_count
 
 
 @app.route('/')
@@ -9,4 +26,4 @@ def home():
 
 @app.route('/user')
 def user():
-    return render_template('user.html')
+    return App.render(render_template('user.html'))

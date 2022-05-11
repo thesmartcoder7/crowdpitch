e810a8e8d52af51035b69da25fc95d61f7aa84c7
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-11 10:45:52 +0300

    patch: fix the sql uri problem

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ class Config():
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace( "postgresql://", "postgres://", 1)

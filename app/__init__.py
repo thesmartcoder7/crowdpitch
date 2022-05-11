3ecc471c6f1ec6daff14bfb79ce45c0dc0c12437
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-12 01:27:01 +0300

    patch: fixing configuration error

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from ..config import ProdConfig
+from config import ProdConfig
 
 
 app = Flask(__name__)
-# use from object. app.config from object
 app.config['SQLALCHEMY_DATABASE_URI'] = ProdConfig.SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
